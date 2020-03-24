f27e2f6dc76d4e47de672a6dd519d6725d88380d
chenjunyuan1996 <chenjunyuan1996@outlook.com>
2020-03-24 09:58:32 +0800

    Add files via upload

---
+++ day5/abstractClasses/Student.java
@@ -0,0 +1,19 @@
+package abstractClasses;
+
+public class Student extends Person {
+
+    private String major;
+    /**
+     * @param name the student's name
+     * @param major the student's major
+     */
+    public Student(String name, String major) {
+        // pass name to superclass constructor
+        super(name);
+        this.major = major;
+    }
+
+    public String getDescription() {
+        return "an student majoring in " + major;
+    }
+}
