e927eeea19a863fa6cfbcbcdf7afbdb25261f8ae
chenjunyuan1996 <chenjunyuan1996@outlook.com>
2020-03-31 16:32:40 +0800

    Create 02_BubbleSort.java

---
+++ day12/sort_algorithm/02_BubbleSort.java
@@ -0,0 +1,16 @@
+// 冒泡排序
+
+class Solution {
+    public int[] sortArray(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 1; j < nums.length - i; j++) {
+                if (nums[j - 1] > nums[j]) {
+                    int tmp = nums[j];
+                    nums[j] = nums[j - 1];
+                    nums[j - 1] = tmp;
+                }
+            }
+        }
+        return nums;
+    }
+}
