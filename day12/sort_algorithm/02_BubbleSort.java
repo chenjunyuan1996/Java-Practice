7ed8c5ac4587159b0ce70986c91c74212a7338c6
chenjunyuan1996 <chenjunyuan1996@outlook.com>
2020-04-05 10:41:20 +0800

    Update 02_BubbleSort.java

--- day12/sort_algorithm/02_BubbleSort.java
+++ day12/sort_algorithm/02_BubbleSort.java
@@ -3,13 +3,16 @@
 class Solution {
     public int[] sortArray(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
+            boolean flag = false;
             for (int j = 1; j < nums.length - i; j++) {
                 if (nums[j - 1] > nums[j]) {
                     int tmp = nums[j];
                     nums[j] = nums[j - 1];
                     nums[j - 1] = tmp;
+                    flag = true;
                 }
             }
+            if (flag == false) break;
         }
         return nums;
     }
