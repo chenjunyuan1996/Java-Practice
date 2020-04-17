6d6646bb3d0d6eb47db2567085df5519eb216130
chenjunyuan1996 <chenjunyuan1996@outlook.com>
2020-04-17 22:27:26 +0800

    Create deleteDuplicates.java

---
+++ day29/deleteDuplicates.java
@@ -0,0 +1,26 @@
+/*
+给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode cur = head;
+        while (cur != null && cur.next != null) {
+            if (cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return head;
+        
+    }
+}
