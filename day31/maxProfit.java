26c845cacd136301dfdf56e248940bbc210f7517
chenjunyuan1996 <chenjunyuan1996@outlook.com>
2020-04-20 10:28:34 +0800

    Create maxProfit.java

---
+++ day31/maxProfit.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices.length < 2) return 0;
+
+        int min = prices[0], max = 0;
+        for (int i = 1; i < prices.length; i++) {
+            max = Math.max(max, prices[i] - min);
+            min = Math.min(min, prices[i]);
+        }
+
+        return max;
+    }
+}
+
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         if (prices.length < 2) return 0;
+
+//         int min = prices[0], max = 0;
+//         for (int i = 1; i < prices.length; i++) {
+//             if (prices[i] < min) {
+//                 min = prices[i];
+//             } else if (prices[i] - min > max) {
+//                 max = prices[i] - min;
+//             }
+//         }
+
+//         return max;
+//     }
+// }
