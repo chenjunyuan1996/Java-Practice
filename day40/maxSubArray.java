625914c6cc1d87898ac7bb9d93b361a830b48155
chenjunyuan1996 <chenjunyuan1996@outlook.com>
2020-04-28 23:39:25 +0800

    Create maxSubArray.java

---
+++ day40/maxSubArray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int cur_sum = nums[0], max_sum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            cur_sum = Math.max(nums[i], cur_sum + nums[i]);
+            max_sum = Math.max(max_sum, cur_sum);
+        }
+
+        return max_sum;
+    }
+}
