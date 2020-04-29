72d26e53d565e331b101cd359911a1f62456c537
chenjunyuan1996 <chenjunyuan1996@outlook.com>
2020-04-29 23:24:20 +0800

    Create minCount.java

---
+++ day41/minCount.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int minCount(int[] coins) {
+        int res = 0;
+        for (int coin : coins) {
+            res += (coin + 1) / 2;
+        }
+        return res;
+    }
+}
