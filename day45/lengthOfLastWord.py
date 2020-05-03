6c8f7a91859740e8344531ee3479e7ccad860128
chenjunyuan1996 <chenjunyuan1996@outlook.com>
2020-05-04 00:01:20 +0800

    Create lengthOfLastWord.py

---
+++ day45/lengthOfLastWord.py
@@ -0,0 +1,10 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        end = len(s) - 1
+        while end >= 0 and s[end] == " ": 
+            end -= 1
+        if end < 0: return 0
+        start = end
+        while start >= 0 and s[start] != " ":
+            start -= 1
+        return end - start
