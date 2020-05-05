aa0c3eda5ffe5e2d54257eb42711986c38c5066a
chenjunyuan1996 <chenjunyuan1996@outlook.com>
2020-05-05 23:15:49 +0800

    Update myPow.py

--- day46/myPow.py
+++ day46/myPow.py
@@ -10,3 +10,17 @@ class Solution:
             i //= 2
 
         return res if n > 0 else 1 / res
+        
+# class Solution:
+#     def myPow(self, x: float, n: int) -> float:
+#         def fastPow(x, n):
+#             if n == 0: return 1
+#             half = fastPow(x, n // 2)
+
+#             if n % 2 == 0: return half * half
+#             else: return half * half * x
+        
+#         if n < 0:
+#             n = -n
+#             x = 1 / x
+#         return fastPow(x, n)
