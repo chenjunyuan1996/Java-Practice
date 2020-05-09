54a946c2dd4a798bb0f97568e2281e389c7eece5
chenjunyuan1996 <chenjunyuan1996@outlook.com>
2020-05-09 18:02:44 +0800

    Update KthLargest.py

--- day49/KthLargest.py
+++ day49/KthLargest.py
@@ -3,9 +3,13 @@ import heapq
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
-        self.heap = heapq.nlargest(k, nums)
-        heapq.heapify(self.heap)
+        # self.heap = heapq.nlargest(k, nums)
+        # heapq.heapify(self.heap)
         self.k = k
+        self.heap = nums[:k]
+        heapq.heapify(self.heap)
+        for i in range(k, len(nums)):
+            self.add(nums[i])
         
     def add(self, val: int) -> int:
         if len(self.heap) < self.k:
