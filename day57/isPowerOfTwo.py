eb8d2d432f4f47ab1910a61ecd9832f2afa753ef
chenjunyuan1996 <chenjunyuan1996@outlook.com>
2020-05-17 18:07:00 +0800

    Create isPowerOfTwo.py

---
+++ day57/isPowerOfTwo.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0: return False
+        return n & (n - 1) == 0
+
+# class Solution:
+#     def isPowerOfTwo(self, n: int) -> bool:
+#         if n <= 0: return False
+#         res = 0
+#         while n != 0:
+#             n &= n - 1
+#             res += 1
+#         return res == 1
