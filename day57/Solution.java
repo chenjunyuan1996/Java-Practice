57933d77afaf7df0b1a2f6839132e50cb7bfb300
chenjunyuan1996 <chenjunyuan1996@outlook.com>
2020-05-17 17:50:57 +0800

    Create Solution.java

---
+++ day57/Solution.java
@@ -0,0 +1,29 @@
+/*
+191. 位1的个数
+编写一个函数，输入是一个无符号整数，返回其二进制表达式中数字位数为 ‘1’ 的个数（也被称为汉明重量）。
+
+*/
+
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int res = 0;
+        for (; n != 0; n &= n - 1) 
+            res++;
+        return res;
+        
+    }
+}
+
+// public class Solution {
+//     // you need to treat n as an unsigned value
+//     public int hammingWeight(int n) {
+//         int res = 0;
+//         while (n != 0) {
+//             n &= n - 1;
+//             res++;
+//         }
+//         return res;
+        
+//     }
+// }
