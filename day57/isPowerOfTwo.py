4a9a4645ae065ffe6173ad10d2ea5a44e79d3bd0
chenjunyuan1996 <chenjunyuan1996@outlook.com>
2020-05-17 18:31:03 +0800

    Update isPowerOfTwo.py

--- day57/isPowerOfTwo.py
+++ day57/isPowerOfTwo.py
@@ -1,7 +1,6 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n <= 0: return False
-        return n & (n - 1) == 0
+        return n > 0 and n & (n - 1) == 0
 
 # class Solution:
 #     def isPowerOfTwo(self, n: int) -> bool:
