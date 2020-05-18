c25f6bbdf54a0f32d73be0dbbef36df6ec8de713
chenjunyuan1996 <chenjunyuan1996@outlook.com>
2020-05-18 14:52:32 +0800

    Create minimumTotal.java

---
+++ day58/minimumTotal.java
@@ -0,0 +1,21 @@
+/*
+120. 三角形最小路径和
+给定一个三角形，找出自顶向下的最小路径和。每一步只能移动到下一行中相邻的结点上。
+相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。
+
+链接：https://leetcode-cn.com/problems/triangle/
+*/
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int[] dp = new int[triangle.size() + 1];
+
+        for (int i = triangle.size() - 1; i > -1; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i).get(j);
+            }
+        }
+
+        return dp[0];
+    }
+}
