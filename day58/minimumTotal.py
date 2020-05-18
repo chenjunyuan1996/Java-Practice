dc1458a8d880935be54bea691766144a8afd8e24
chenjunyuan1996 <chenjunyuan1996@outlook.com>
2020-05-18 14:59:53 +0800

    Update minimumTotal.py

--- day58/minimumTotal.py
+++ day58/minimumTotal.py
@@ -1,7 +1,7 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         dp = triangle[-1]
-        for i in range(len(triangle) - 1, 0, -1):
-            for j in range(i):
-                dp[j] = min(dp[j], dp[j + 1]) + triangle[i - 1][j]
+        for i in range(len(triangle) - 2, -1, -1):
+            for j in range(i + 1):
+                dp[j] = min(dp[j], dp[j + 1]) + triangle[i][j]
         return dp[0]
