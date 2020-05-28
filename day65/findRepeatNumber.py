865557c680b1ad89c74505b3cbdf02a8293cffd8
chenjunyuan1996 <chenjunyuan1996@outlook.com>
2020-05-28 22:24:04 +0800

    Update findRepeatNumber.py

--- day65/findRepeatNumber.py
+++ day65/findRepeatNumber.py
@@ -1,12 +1,31 @@
+# class Solution:
+#     def findRepeatNumber(self, nums: List[int]) -> int:
+#         for i, num in enumerate(nums):
+#             while num != i:
+#                 if num == nums[num]:
+#                     return num
+#                 nums[i], nums[num] = nums[num], nums[i]
+
+# 重排数组，把数组数字和下标对应上
 class Solution:
     def findRepeatNumber(self, nums: List[int]) -> int:
-        dic = set()
-        for num in nums:
-            if num in dic:
+        for i, num in enumerate(nums):
+            if num == i: continue
+            if nums[num] == num:
                 return num
-            dic.add(num)
-        return -1
+            nums[num], nums[i] = nums[i], nums[num]
 
+# 采用哈希表，增加了空间复杂度
+# class Solution:
+#     def findRepeatNumber(self, nums: List[int]) -> int:
+#         dic = set()
+#         for num in nums:
+#             if num in dic:
+#                 return num
+#             dic.add(num)
+#         return -1
+
+# 暴力搜索
 # class Solution:
 #     def findRepeatNumber(self, nums: List[int]) -> int:
 #         for i, target in enumerate(nums):
@@ -15,6 +34,7 @@ class Solution:
 #                     return target
 #         return -1
 
+# 从小到大排序数组，寻找相邻两个相似的值
 # class Solution:
 #     def findRepeatNumber(self, nums: List[int]) -> int:
 #         nums.sort()
