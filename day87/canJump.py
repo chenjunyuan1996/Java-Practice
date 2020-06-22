2f9a5dfe902f7466dcc6b84a91f74fe2c9faa451
chenjunyuan1996 <chenjunyuan1996@outlook.com>
2020-06-22 20:03:06 +0800

    Create canJump.py

---
+++ day87/canJump.py
@@ -0,0 +1,8 @@
+# 计算最远能到达的地方
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        length = 0
+        for i, jump in enumerate(nums):
+            if length >= i and i + jump > length:
+                length = i + jump
+        return length >= i
