3186038eee7eb4e4dcea59afc99d41eb21d07532
chenjunyuan1996 <chenjunyuan1996@outlook.com>
2020-06-29 10:11:14 +0800

    Create findKthLargest.py

---
+++ day94/findKthLargest.py
@@ -0,0 +1,12 @@
+import heapq
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heapq.heapify(nums)
+        for _ in range(len(nums) - k):
+            heapq.heappop(nums)
+        return heapq.heappop(nums)
+
+# class Solution:
+#     def findKthLargest(self, nums: List[int], k: int) -> int:
+#         nums.sort()
+#         return nums[len(nums) - k]
