afc526cbe2f5d0d0ea6485fb798ee6eda16693b0
chenjunyuan1996 <chenjunyuan1996@outlook.com>
2020-07-06 20:50:12 +0800

    Create longestValidParentheses.java

---
+++ day99/longestValidParentheses.java
@@ -0,0 +1,28 @@
+/*
+32. 最长有效括号
+给定一个只包含 '(' 和 ')' 的字符串，找出最长的包含有效括号的子串的长度。
+
+链接: https://leetcode-cn.com/problems/longest-valid-parentheses/
+*/
+
+class Solution {
+    public int longestValidParentheses(String s) {
+        int res = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    res = Math.max(res, i - stack.peek());
+                }
+            }
+        }
+        return res;
+
+    }
+}
