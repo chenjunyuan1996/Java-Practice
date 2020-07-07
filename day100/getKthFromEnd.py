184793511e2afb6239b1d22bdc4533fb5b8c1ae7
chenjunyuan1996 <chenjunyuan1996@outlook.com>
2020-07-07 12:08:18 +0800

    Create getKthFromEnd.py

---
+++ day100/getKthFromEnd.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getKthFromEnd(self, head: ListNode, k: int) -> ListNode:
+        fast = head
+        slow = head
+        for _ in range(k):
+            fast = fast.next
+        
+        while fast:
+            slow = slow.next
+            fast = fast.next
+        return slow
